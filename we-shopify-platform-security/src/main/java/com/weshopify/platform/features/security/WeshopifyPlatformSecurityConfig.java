3db09f06856fc106e0dec5e4a460a79de1b33e22
GAYAS <GAYAS@HP-15s-FR2006TU>
2022-10-30 07:31:46 +0530

    Added Security to Simple Monolithic application

---
+++ we-shopify-platform-security/src/main/java/com/weshopify/platform/features/security/WeshopifyPlatformSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.weshopify.platform.features.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WeshopifyPlatformSecurityConfig extends WebSecurityConfigurerAdapter
+{
+	@Autowired
+	private WeshopifyUserDeatilsService userDetailsService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
+	{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception 
+	{
+		http
+		.authorizeHttpRequests()
+		.antMatchers("/","/home","/selfReg-customer","/selfReg","/error")
+		.permitAll()
+		.antMatchers("/customer-admin-reg",
+				"/customer",
+				"/view-customers",
+				"/view-customers/**",
+				"/delete-customers",
+				"/delete-customers/**",
+				"/create-customer",
+				"/edit-customers/",
+				"/search-customers",
+				"/access-denied")
+		.authenticated()
+		.and().csrf().disable()
+		.formLogin()
+		.loginPage("/login")
+		.defaultSuccessUrl("/view-customers")
+		.usernameParameter("userName")
+		.passwordParameter("password")
+		.and()
+		.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/home")
+		.and()
+		.exceptionHandling()
+		.accessDeniedPage("/access-denied");
+
+	}
+}
---
+++ we-shopify-platform-security/src/main/java/com/weshopify/platform/features/security/WeshopifyUserDeatilsService.java
@@ -0,0 +1,50 @@
+package com.weshopify.platform.features.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.weshopify.platform.features.customers.models.Customer;
+import com.weshopify.platform.features.customers.repository.CustomerDataRepo;
+import com.weshopify.platform.features.customers.service.CustomerService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class WeshopifyUserDeatilsService implements UserDetailsService {
+
+	@Autowired
+	private CustomerDataRepo customerDataRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.info("Loggedin User :" + username);
+		
+		Customer customer  = customerDataRepo.searchByEmail(username).get(0);
+		
+		String loggedInUser	=	customer.getEmail();
+		log.info("User Fetched from DB : "+ loggedInUser);
+		
+		String passsword	=	customer.getPassword();
+		log.info("Password Fetched from DB : "+ passsword);
+		
+		String roleName	=	customer.getRole().getRole();
+		log.info("Role Name of Customer : "+ roleName);
+		
+		List<GrantedAuthority> authorityList = new ArrayList<>();
+		authorityList.add(new SimpleGrantedAuthority(roleName));
+		
+		return new User(username, passsword, authorityList);
+	}
+
+}
